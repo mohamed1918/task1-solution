887ddd3f30b20f6553d8bfaebe85c63b27451036
mohos <mohos@DESKTOP-UT0B80T>
2025-06-21 23:22:20 +0300

    Add project files.

---
+++ task1/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Point
+    {
+        public int x;
+        public int y;
+
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"({x},{y})";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            //return base.Equals(obj);
+            return x == ((Point)obj).x && y == ((Point)obj).y ;
+        }
+    }
+
+    internal class Point3D : Point
+    {
+        public int z;
+    }
+
+    struct Temp
+    {
+
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.Serialization;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main()
+        
+        {
+            #region Erros
+            //int x;
+            
+            //y = 10;
+
+            //Console.WriteLine(5 / 0);
+            
+            #endregion
+
+            #region Reference data
+            //Point P1;
+
+            //P1 = new Point();
+
+            //Point P2 = new Point();
+
+            //P1.x = 1;
+            //P1.y = 2;
+
+            //P2.x = 3;   
+            //P2.y = 4;
+            //Console.WriteLine($"P1 = ({P1.x},{P1.y})");
+            //Console.WriteLine($"P2 = ({P2.x},{P2.y})");
+
+            //P1 = P2;
+
+            //P1.x = 8;
+            //P2.y = 10;
+            //Console.WriteLine($"P1 = ({P1.x},{P1.y})");
+            //Console.WriteLine($"P2 = ({P2.x},{P2.y})");
+
+            #endregion
+
+            #region Object
+            ////Point3D P3D = new Point3D();
+
+            ////P3D.x = 1;
+            ////P3D.y = 2;  
+            ////P3D.z = 3;
+
+            //Point p1;
+
+            //p1 = new Point();
+            ////p1 = new Point3D();
+
+            //object obj;
+            //obj = new object();
+            //obj = new Point();
+            //obj = new Point3D();
+            //obj = new Temp();
+            //obj = 123;
+            //obj = 9.4;
+            //obj = 'm';
+            //obj = "mohammed hossam";
+            //obj = 3;
+
+            //int x = 1,y = 1;
+            //int sum;
+            //string s = "mohammed hossam";
+
+            //// sum = x + y + s; // error type
+            ////sum = x + y + (int)obj;
+            ////Console.WriteLine(sum); // 7
+
+
+
+            ////Console.WriteLine(x.ToString());
+            ////Console.WriteLine(s.ToString());
+            ////Console.WriteLine(p1.ToString());
+
+            //string m = "mohammed hossam";
+            //Point p2 = new Point();
+
+
+            ////Console.WriteLine(x.Equals(y));
+            ////Console.WriteLine(s.Equals(m));
+            ////Console.WriteLine(p1.Equals(p2));
+
+
+
+
+            ////Console.WriteLine(x.GetHashCode());
+            ////Console.WriteLine(s.GetHashCode());
+            ////Console.WriteLine(m.GetHashCode());
+            ////Console.WriteLine(p1.GetHashCode());
+            ////Console.WriteLine(p2.GetHashCode());
+
+
+
+
+            ////Console.WriteLine(x.GetType());
+            ////Console.WriteLine(s.GetType());
+            ////Console.WriteLine(p1.GetType());
+
+            ////Print(5, 7);
+            ////Print('a', 'b');
+            ////Print(5.5, 3.5);
+
+
+            #endregion
+        }
+
+        // static void Print(object x, object y) 
+        // {
+        //     Console.WriteLine(x);
+        //     Console.WriteLine(y);
+        //     ;
+        //}
+
+    }
+}
+    
